55a6d43c0190b09cb5b550c0f326b9d9177f4b1f
martinRenou <martin.renou@gmail.com>
2022-04-07 14:04:54 +0200

    Release 0.2.4

--- bqscales/_version.py
+++ bqscales/_version.py
@@ -15,5 +15,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (0, 2, 3)
+version_info = (0, 2, 4)
 __version__ = ".".join(map(str, version_info))
